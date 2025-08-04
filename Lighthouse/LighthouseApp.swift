d0dc6f1db679fea8aec89dd4f23d42084962522a
Havard <haavardhaugers@icloud.com>
2025-08-04 11:35:49 +0200

    Initial Commit

---
+++ Lighthouse/LighthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseApp.swift
+//  Lighthouse
+//
+//  Created by havard.sjong@vipps.no on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
